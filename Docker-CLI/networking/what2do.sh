da91bad8252766fcb0eaa7bd08c8f9cc026e1960
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2022-11-04 21:21:42 +0100

    feat: updated Earthly tutorial

--- Docker-CLI/networking/what2do.sh
+++ Docker-CLI/networking/what2do.sh
@@ -7,13 +7,6 @@ docker network create --driver bridge alpine-net
 docker run -dit --name alpine1 --network alpine-net alpine ash
 docker run -dit --name alpine2 --network alpine-net alpine ash
 docker run -dit --name alpine3 alpine ash
-docker run -dit --name alpine4 --network alpine-net alpine ash
-
-# connect alpine4 container to bridge network
-docker network connect bridge alpine4
-
-# inspect bridge network
-docker network inspect bridge
 
 # inspect alpine-net network
 docker network inspect alpine-net
@@ -26,7 +19,7 @@ docker container attach alpine1
     # CTRL-P CTRL-Q for detach
 
 # connect to other container to check network connection
-docker container attach alpine4
+docker container attach alpine3
     # ping -c 2 alpine2
     # ping -c 2 alpine3
     # ping -c 2 google.com
